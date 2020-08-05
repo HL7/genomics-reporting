a6199b582ba56b05024ede71658119a378a875d1
Kevin Power <kpower@cerner.com>
2020-08-05 15:33:56 -0500

    Upgrade IG to new template
    Upgrade IG to new template (hl7.fhir.template)

--- _genonce.sh
+++ _genonce.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
-export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
-java -jar framework/ant/ant-launcher.jar -buildfile framework/build.xml -DupdatePublisher=N
+publisher_jar=publisher.jar
+input_cache_path=./input-cache/
+echo Checking internet connection...
+curl -sSf tx.fhir.org > /dev/null
+
+if [ $? -eq 0 ]; then
+	echo "Online"
+	txoption=""
+else
+	echo "Offline"
+	txoption="-tx n/a"
+fi
+
+echo "$txoption"
+
+publisher=$input_cache_path/$publisher_jar
+if test -f "$publisher"; then
+	java -jar $publisher -ig ig.ini $txoption $*
+
+else
+	publisher=../$publisher_jar
+	if test -f "$publisher"; then
+		java -jar $publisher -ig ig.ini $txoption $*
+	else
+		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
+	fi
+fi
--- _genonceNoTx.bat
+++
@@ -1,3 +0,0 @@
-SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8 -DupdatePublisher=N
-java -jar framework\ant\ant-launcher.jar -buildfile framework/build.xml -DnoTx=Y
-pause
